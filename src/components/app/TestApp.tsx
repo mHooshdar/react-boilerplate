a87b56f6a9bc33c3b6e45fa43645ceaf91bc61ef
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-04-03 19:19:37 +0430

    chore: add test component

---
+++ src/components/app/TestApp.tsx
@@ -0,0 +1,10 @@
+// node_modules
+import React from 'react';
+
+type AllProps = {};
+
+const TestApp: React.FC<AllProps> = () => {
+  return <div>sad</div>;
+};
+
+export default TestApp;
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,10 +1,17 @@
 // node_modules
 import React from 'react';
+// components
+import TestApp from '@components/app/TestApp';
 
 type AllProps = {};
 
 const AppIndexPage: React.FC<AllProps> = () => {
-  return <div className="a">app</div>;
+  return (
+    <div className="a">
+      app
+      <TestApp />
+    </div>
+  );
 };
 
 export default AppIndexPage;
